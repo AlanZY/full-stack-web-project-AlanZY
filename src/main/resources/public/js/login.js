ab817c90a0f8dca0b28a8221fa29bf7c96baf046
AlanZY <ziyi.lan814@gmail.com>
2016-02-17 11:17:14 -0500

    0217

--- src/main/resources/public/js/login.js
+++ src/main/resources/public/js/login.js
@@ -1,19 +1,4 @@
-function show()
-{
-div1.style.display="inline";
-div1.style.width=body.clientWidth;
-div1.style.height=body.clientHeight;
-div2.style.display="inline";
-div2.style.top=body.clientHeight/2-div2.clientHeight/2;
-div2.style.left=body.clientWidth/2-div2.clientWidth/2;
-}
 
-//close the window
-function closeShow()
-{
-div1.style.display="none";
-div2.style.display="none";
-}
 
 
 function sign_switch(){  
@@ -26,3 +11,28 @@ function sign_switch(){
 	  document.getElementById('sign_up_color').className = 'sign_appear_color';
 	 
 }  
+
+function login_switch(){  
+
+// hide login in 
+// show sign up
+	 document.getElementById('login_in').className = 'login_in';
+	 document.getElementById('sign_up').className = 'sign_up';
+	  document.getElementById('login_in_color').className = 'login_in_wrap_up';
+	  document.getElementById('sign_up_color').className = 'sign_up_wrap_up';
+	 
+}  
+
+var url = '#'; 
+function show(evt,o){ 
+
+evt.stopPropagation?evt.stopPropagation():evt.cancelBubble=true;
+var o = document.getElementById(o); 
+o.style.display = ""; 
+} 
+function hide(o){ 
+var o = document.getElementById(o); 
+o.style.display = "none"; 
+window.location = url; 
+} 
+document.onclick=function(){hide('div2');}
