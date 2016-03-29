81cf0b75dd9be3550b23ee46721cd19e1c8e1f70
AlanZY <ziyi.lan814@gmail.com>
2016-03-29 09:25:41 -0400

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -167,14 +167,14 @@ public class Main {
 
 
                     Statement stmt = connection.createStatement();
-                    stmt.executeUpdate("create table if not exists users (email_address email, password text)");
+                    stmt.executeUpdate("create table if not exists users (email_address int, password int)");
                   //  stmt.executeUpdate("insert into users" +
                   //           "(email_address, password)" +
                   //          "values('" + email + "','" + password + "')");
 
                   stmt.executeUpdate("insert into users" +
                         "(email_address, password)" +
-                         "values('emailhb','jfvdkjn')");
+                         "values(23,4)");
                     ResultSet rs = stmt.executeQuery("select email_address from users");
 
                    ArrayList<String> output = new ArrayList<String>();
