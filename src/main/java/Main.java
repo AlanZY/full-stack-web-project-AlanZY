46c2f9672c3c159c03031a71cebd9d167aaf37f6
AlanZY <ziyi.lan814@gmail.com>
2016-03-29 02:06:45 -0400

    test db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -160,7 +160,7 @@ public class Main {
                     Map<String, Object> attributes = new HashMap<>();
                     try{
                     connection = DatabaseUrl.extract().getConnection();
-                    JSONObject obj = new JSONObject(req.body());
+                  //  JSONObject obj = new JSONObject(req.body());
                   //  String email = obj.getString("signin-email");
                   //  String password = obj.getString("signin-password");
                   //  System.out.println(email);
