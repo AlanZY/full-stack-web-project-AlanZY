d3a08b92c5fd3fea10c92ac0525eea44e56ebfd2
AlanZY <ziyi.lan814@gmail.com>
2016-03-29 13:57:13 -0400

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -87,12 +87,11 @@ public class Main {
   //    stmt.executeUpdate("INSERT INTO users_test VALUES ('user1')");
       ResultSet rs = stmt.executeQuery("SELECT * FROM users_test");
      ArrayList<String> output = new ArrayList<String>();
-     int i=1;
-    while(rs.next())
-    {
 
-       output.add("read from users, " + "email: " + rs.getString(i++) );
-     }
+
+       output.add("read from users, " + "email: " + rs.getString(1) );
+       output.add("read from users, " + "email: " + rs.getString(2) );
+
     attributes.put("results",output);
      return new ModelAndView(attributes, "testdb.ftl");
      } catch (Exception e) {
