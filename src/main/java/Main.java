ec585c4c861a292c4b666b1d1b66f7609c71d08d
AlanZY <ziyi.lan814@gmail.com>
2016-03-29 21:04:43 -0400

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -119,8 +119,8 @@ public class Main {
         connection = DatabaseUrl.extract().getConnection();
 
        JSONObject obj = new JSONObject(req.body());
+ 
 
-         
         String email = obj.getString("command");
     //    String password = obj.getString("signin-password");
 
