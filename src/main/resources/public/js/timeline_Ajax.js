17297ad1e23117710e1262b66fd7ee8d1b4ef5ea
AlanZY <ziyi.lan814@gmail.com>
2016-04-27 23:58:53 -0400

    image

--- src/main/resources/public/js/timeline_Ajax.js
+++ src/main/resources/public/js/timeline_Ajax.js
@@ -7,9 +7,8 @@ $(function()
       url:"/api/timeline_info",
       success: function(result)
       {
-        alert(result);
+        alert("this is a timeline page");
         var result1=JSON.parse(result);
-        alert(result1);
         $("#h1").html(result1.title1);
         $("#p1").html(result1.content1);
         var image1=document.getElementById("image1");
